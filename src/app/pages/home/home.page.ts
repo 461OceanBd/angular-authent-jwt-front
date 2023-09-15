e4adde537186280389b0e05bb85ecb2a89c33e32
Romain Mercier <romain.mercier321@gmail.com>
2023-09-15 15:19:50 +0200

    Home-page :
    ajout d'un arriere plan réactif au mouvement de la souris

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.page.html',
   styleUrls: ['./home.page.scss']
 })
-export class HomePage {
+export class HomePage implements AfterViewInit {
+
+  constructor() { }
+
+  ngAfterViewInit(): void {
+    const element = document.querySelector('.backgound-container') as HTMLElement;
+    const movementFactor = 10; // move sensibility
+
+    if (element) {
+      element.addEventListener('mousemove', (event) => {
+        const mouseX = event.clientX / window.innerWidth;
+        const mouseY = event.clientY / window.innerHeight;
+
+        const translateX = (mouseX - 0.5) * movementFactor;
+        const translateY = (mouseY - 0.5) * movementFactor;
+
+        element.style.backgroundPositionX = `${translateX}px`;
+        element.style.backgroundPositionY = `${translateY}px`;
+      });
+    }
+  }
+
+
 
 }
