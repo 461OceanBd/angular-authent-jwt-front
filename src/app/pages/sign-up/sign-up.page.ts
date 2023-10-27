4b0989214dd4fdc05dec18f3e31102c84e832b8b
Romain Mercier <romain.mercier321@gmail.com>
2023-10-27 12:20:22 +0200

    Sign-up :
    Mise en place du process d'enregistrement d'un user. Appel du endpoint,
    navigation vers une route (authguard et logique de navigation selon le
    rôle du user a TODO). Gestion d'erreur et des toasts correspondants

--- src/app/pages/sign-up/sign-up.page.ts
+++ src/app/pages/sign-up/sign-up.page.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { catchError, of, switchMap, tap } from 'rxjs';
+import { User } from 'src/app/models/user.model';
+import { AuthService } from 'src/app/services/auth.service';
+import { ToastService } from 'src/app/services/toast.service';
 import { PASSWORD_PATTERN } from 'src/app/utils/validation-patterns';
 import { matchPasswordValidator } from 'src/app/utils/validators/match-password.validator';
 
@@ -17,7 +22,11 @@ export class SignUpPage implements OnInit {
     repassword: this.fb.control('', [Validators.required, matchPasswordValidator('password')]),
    }, {updateOn: 'submit'});
 
-  constructor(private fb: FormBuilder) { }
+  constructor(
+    private fb: FormBuilder,
+    private authService: AuthService,
+    private router: Router,
+    private toast: ToastService) { }
 
   ngOnInit(): void {
   }
@@ -25,6 +34,22 @@ export class SignUpPage implements OnInit {
   public onSubmit(): void {
     this.submitted = true;
     if (this.signUpForm.valid) {
+      const user = new User(this.email?.value, this.password?.value);
+      this.authService.createUser(user).pipe(
+        switchMap((createdUser: User) =>{
+          if (!!createdUser) {
+            this.router.navigateByUrl('autre'); // TODO: affichage dashboard selon profile
+            this.toast.showSuccess('Profil créé avec succès', 'Well done !')
+          }
+          return of(createdUser);
+        }),
+        catchError((err) => {
+          const toastTitle  = 'Erreur ' + err.status;
+          const toastMessage = err.error;
+          this.toast.showError(toastMessage, toastTitle);
+          return of(null);
+        })
+      ).subscribe();
 
     }
 
