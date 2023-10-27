fc3faf145de2bfabe0179a921a1cf1ab64ace393
Romain Mercier <romain.mercier321@gmail.com>
2023-10-27 12:05:21 +0200

    Technique :
    - Ajout des config d'environnement
    - Ajout de la structure d'un interceptor pour ajout header request
    - ajout ngx-toastr composant

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,10 @@ import { NotFoundComponent } from './components/not-found/not-found.component';
 import { SignUpPage } from './pages/sign-up/sign-up.page';
 import { SignInPage } from './pages/sign-in/sign-in.page';
 import { ReactiveFormsModule } from '@angular/forms';
+import { httpInterceptorProviders } from './interceptors';
+import { HttpClientModule } from '@angular/common/http';
+import { ToastrModule } from 'ngx-toastr';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -23,8 +27,13 @@ import { ReactiveFormsModule } from '@angular/forms';
     BrowserModule,
     ReactiveFormsModule,
     AppRoutingModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    ToastrModule.forRoot(),
   ],
-  providers: [],
+  providers: [
+    httpInterceptorProviders
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: true,
+  baseApiUrl: 'https://myurl.com' // TODO: replace by prod api url
+};
